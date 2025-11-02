2c9c7775424cfa172c91427747cb9bdeada870b3
mannavadolatejaswi-hue <mannavadolatejaswi@gmail.com>
2025-11-02 17:01:27 +0530

    Main.c
    Calculate attendance eligibility based on student input.

--- day9-smart attendance tracker/Main.c
+++ day9-smart attendance tracker/Main.c
@@ -1,1 +1,31 @@
-
+#include <stdio.h>
+int main() {
+    int n, i, present;
+    float percentage, totalPercentage;
+    int eligible = 0, notEligible = 0;
+    const int totalDays = 30;
+    printf("Enter total number of students: ");
+    scanf("%d", &n);
+    i = 1;
+    while (i <= n) {
+        printf("Enter attendance for student %d : ", i);
+        scanf("%d", &present);
+        percentage = (present / 30) * 100;
+        totalPercentage += percentage;
+        if (percentage >= 75) {
+            printf("Student %d\n", i);
+            printf("percentage %.2f%%-eligible",percentage);
+            eligible++;
+        } else {
+            printf("Student %d: %.2f%% - Not Eligible\n", i, percentage);
+            notEligible++;
+        }
+        i++;
+    }
+    printf("\nClass Summary:\n");
+    printf("Total Students: %d\n", n);
+    printf("Eligible Students: %d\n", eligible);
+    printf("Not Eligible: %d\n", notEligible);
+    printf("Average Attendance: %.2f%%\n", totalPercentage / n);
+    return 0;
+}
